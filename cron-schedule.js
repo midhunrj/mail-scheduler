2388e3071a011153d2f4607a1e95181fcbd2bc8a
midhunrj <midhunrj18852@gmail.com>
2026-04-25 11:51:13 +0530

    commit push

--- cron-schedule.js
+++ cron-schedule.js
@@ -2,8 +2,10 @@ const cron = require("node-cron");
 const { sendEmails } = require("./mailer");
 
 const startEmailScheduler=(emailList)=>{
+  console.log("hello i am sending");
+  
     cron.schedule(
- "45 11 * * *",
+ "55 11 * * *",
   async () => {
     console.log("Running at 11:45 IST");
     await sendEmails(emailList);
